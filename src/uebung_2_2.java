77c461833261ae0ac52d9e0bef75dcb606e1ff0b
14605 <14605@A101-05.nordakademie.intern>
2016-10-21 11:27:10 +0200

    huhuhuhu

---
+++ src/uebung_2_2.java
@@ -0,0 +1,18 @@
+
+public class uebung_2_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a = 8;
+		double b = 3;
+		
+		double var1 = a*a+2*a*b+b*b;
+		double var2 = (a+b)*(a+b);
+	    System.out.println("x1= "+ var1);
+	    System.out.println("x2= "+ var2);
+
+
+
+	}
+
+}
